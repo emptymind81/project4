507a309a0f50a5149e23df44f68b2199222d189e
Emptymind <dreambuild@foxmail.com>
2013-08-22 02:42:16 +0800

    add drag color button

---
+++ Dulux/UIDraggableView.h
@@ -0,0 +1,13 @@
+//
+//  UIDraggableView.h
+//  Dulux
+//
+//  Created by Emptymind on 8/22/13.
+//  Copyright (c) 2013 dangdang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDraggableView : UIView
+
+@end
---
+++ Dulux/UIDraggableView.m
@@ -0,0 +1,31 @@
+//
+//  UIDraggableView.m
+//  Dulux
+//
+//  Created by Emptymind on 8/22/13.
+//  Copyright (c) 2013 dangdang. All rights reserved.
+//
+
+#import "UIDraggableView.h"
+
+@implementation UIDraggableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
