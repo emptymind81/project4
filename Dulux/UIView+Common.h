ac836fabbac8398eb7f879a7bf9034089d811e90
Emptymind <dreambuild@foxmail.com>
2013-08-25 17:15:21 +0800

    fix 3 bugs

---
+++ Dulux/UIView+Common.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Common.h
+//  Dulux
+//
+//  Created by Emptymind on 8/25/13.
+//  Copyright (c) 2013 dangdang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIView_Common : NSObject
+
+@end
---
+++ Dulux/UIView+Common.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Common.m
+//  Dulux
+//
+//  Created by Emptymind on 8/25/13.
+//  Copyright (c) 2013 dangdang. All rights reserved.
+//
+
+#import "UIView+Common.h"
+
+@implementation UIView_Common
+
+@end
